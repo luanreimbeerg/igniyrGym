32f88f0d9c6031bcdf60dcad7f8b52513851d909
Luan Trajano <luan.trajano@biz.com.br>
2023-02-01 23:25:26 -0300

    feat: finalizando tela de signIn e criando tela de SingUp

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,17 +1,16 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 import { useNavigation } from "@react-navigation/native";
-import { AuthNagigatorRoutesProps } from "@routes/auth.routes";
 
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
 import Input from "@components/Input";
 import Button from "@components/Button";
 
-export default function SignIn() {
-  const navigation = useNavigation<AuthNagigatorRoutesProps>();
+export default function SignUp() {
+  const navigation = useNavigation();
 
-  function handleNewAccount() {
-    navigation.navigate("signUp");
+  function handleGoBack() {
+    navigation.goBack();
   }
 
   return (
@@ -19,7 +18,7 @@ export default function SignIn() {
       contentContainerStyle={{ flexGrow: 1 }}
       showsHorizontalScrollIndicator={false}
     >
-      <VStack flex={1} px={10}>
+      <VStack flex={1} bg="gray.700" px={10}>
         <Image
           source={BackgroundImg}
           defaultSource={BackgroundImg}
@@ -36,28 +35,25 @@ export default function SignIn() {
 
         <Center>
           <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-            Acesse sua conta
+            Crie sua conta
           </Heading>
+          <Input placeholder="Nome" />
           <Input
             placeholder="E-mail"
             keyboardType="email-address"
             autoCapitalize="none"
           />
           <Input placeholder="Senha" secureTextEntry />
-          <Button title="Acessar" />
+          <Input placeholder="Confirme  a Senha" secureTextEntry />
+          <Button title="Criar e acessar" />
         </Center>
 
-        <Center mt={24}>
-          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
-            Ainda não tem acesso?
-          </Text>
-
-          <Button
-            title="Criar conta"
-            variant="outline"
-            onPress={handleNewAccount}
-          />
-        </Center>
+        <Button
+          title="Voltar para o login"
+          variant="outline"
+          mt={24}
+          onPress={handleGoBack}
+        />
       </VStack>
     </ScrollView>
   );
