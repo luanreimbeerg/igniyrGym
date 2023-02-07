4dd8efd0d5b119a0eba5d0bf75037bfe26937c0c
Luan Trajano <luan.trajano@biz.com.br>
2023-02-07 11:11:24 -0300

    feat: routes public

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { Box, useTheme } from "native-base";
+import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
 
 export default function Routes() {
@@ -10,7 +11,7 @@ export default function Routes() {
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   );
